f66b284232865a035dbb1497ba4273acd49fb1e2
the-abe-train <abraham.train@gmail.com>
2021-06-08 22:43:55 -0400

    Moved head tag to index.html

--- src/App.svelte
+++ src/App.svelte
@@ -60,16 +60,6 @@
   };
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-    integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  />
-</svelte:head>
-
 <main>
   <Navbar />
   <Header />
