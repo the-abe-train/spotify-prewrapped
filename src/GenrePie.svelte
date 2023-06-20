a4593cac0a037c45fdcbce640aa674314e34c330
Isen K <isen.kasa@jci.com>
2023-06-20 16:04:27 -0500

    Pie Chart for Genre

--- src/GenrePie.svelte
+++ src/GenrePie.svelte
@@ -4,17 +4,15 @@ import { onMount } from 'svelte';
 
 export let genreData
 
-console.log('genreData:', genreData)
+let chartValues = []
+let chartLabels = []
 
-let chartValues = [20, 10, 5, 2, 20, 30, 45];
-let chartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-// let chartValues = []
-// let chartLabels = []
-
-// for (let data in genreData) {
-//     chartValues.push[data[0]]
-//     chartValues.push(data[1])
-// }
+async function getData () {
+  for (let data of genreData) {
+    chartLabels.push(data[0])
+    chartValues.push(data[1])
+  }
+}
 
 let ctx;
 let chartCanvas;
@@ -23,20 +21,21 @@ function generateCoolColors(num) {
   let colors = [];
   
   for (let i = 0; i < num; i++) {
-    let r = Math.floor(Math.random() * 128);
-    let g = Math.floor(Math.random() * 128);
-    let b = Math.floor(Math.random() * 256);
+    let r = Math.floor(Math.random() * 60);
+    let g = Math.floor(Math.random() * 60);
+    let b = Math.floor(Math.random() * 60);
     colors.push(`rgb(${r}, ${g}, ${b})`);
   }
 
   return colors;
 }
 
-const colors = generateCoolColors(10)
+const colors = generateCoolColors(50)
 
 onMount(async (promise) => {
+  await getData()
   ctx = chartCanvas.getContext('2d');
-  var chart = new chartjs(ctx, {
+  new chartjs(ctx, {
     type: 'pie',
     data: {
       labels: chartLabels,
